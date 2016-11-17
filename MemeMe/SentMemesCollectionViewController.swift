05908ac837662d7cb9a238bc3ec27a5e2a98eb5c
Midhet Sulemani <midhetsulemani@Midhets-MacBook-Air.local>
2016-11-17 08:38:35 +0300

    completed

--- MemeMe/SentMemesCollectionViewController.swift
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -17,25 +17,83 @@ class SentMemesCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        rotated()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
-        let space: CGFloat = 1.0
-        let widthDimension = (self.view.frame.width - (2*space))/3.0
-        let heightDimension = (self.view.frame.height - (2*space))/5.0
-        flowLayout.minimumInteritemSpacing = space
-        flowLayout.minimumLineSpacing = space
-        flowLayout.itemSize = CGSize(width: widthDimension, height: heightDimension)
+        
         
         // Register cell classes
 //        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "sentMemes")
 
         // Do any additional setup after loading the view.
     }
+    
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        
+//        super.viewWillTransition(to: size, with: coordinator)
+//        coordinator.animate(alongsideTransition: nil, completion: {handler in
+//            print("orientation changed")
+//        })
+//    }
+    
+//    override func viewWillTransitionToSize(to: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransition(to: to, with: coordinator)
+//        coordinator.animate(alongsideTransition: nil, completion: {
+//            _ in
+//            
+//            print("orientation changed")
+//        })
+//    }
 
     override func viewWillAppear(_ animated: Bool) {
         collectionView!.reloadData()
+        NotificationCenter.default.addObserver(self, selector: #selector(SentMemesCollectionViewController.rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    func rotated()
+    {
+        if(UIDeviceOrientationIsLandscape(UIDevice.current.orientation))
+        {
+            print("landscape")
+            setCollectionFlowLayout(orientation: "Landscape")
+        }
+        
+        if(UIDeviceOrientationIsPortrait(UIDevice.current.orientation))
+        {
+            print("Portrait")
+            setCollectionFlowLayout(orientation: "Portrait")
+        }
+        
+    }
+    
+    func setCollectionFlowLayout(orientation: String) {
+        
+        if orientation == "Portrait" {
+            let space: CGFloat = 1.0
+            let widthDimension = (self.view.frame.width - (2*space))/3.0
+            let heightDimension = (self.view.frame.height - (2*space))/5.0
+            flowLayout.minimumInteritemSpacing = space
+            flowLayout.minimumLineSpacing = space
+            flowLayout.itemSize = CGSize(width: widthDimension, height: heightDimension)
+            
+        }
+        else if orientation == "Landscape" {
+            let space: CGFloat = 1.0
+            let widthDimension = (self.view.frame.width - (2*space))/5.0
+            let heightDimension = (self.view.frame.height - (2*space))/3.0
+            flowLayout.minimumInteritemSpacing = space
+            flowLayout.minimumLineSpacing = space
+            flowLayout.itemSize = CGSize(width: widthDimension, height: heightDimension)
+            
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+//        self.removeObserver(defa, forKeyPath: )
     }
     
     // MARK: UICollectionViewDataSource
